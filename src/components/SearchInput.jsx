6dcaf7130a6eb4be3894a9c62990d2aea6a95da6
jeongmin <lee.jeongmin1625@gmail.com>
2022-09-20 15:13:15 +0900

    feat: styledcomponents 로 변환

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import styled from "styled-components";
 
 function SearchInput() {
   const [searchText, setSearchText] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    setSearchText(searchParams.get("q"));
+  }, [searchParams]);
   const onChangeInput = (e) => {
     setSearchText(e.target.value);
   };
   const onKeyUp = (e) => {
     if (e.key === "Enter" && e.target.value.trim().length > 0) {
-      setSearchParams({});
+      setSearchParams({ q: e.target.value });
     }
   };
 
@@ -18,7 +23,7 @@ function SearchInput() {
       <input
         value={searchText}
         type="text"
-        className="w-[100px] h-11 bg-slate-50 border ouline-none p-6 text-black mt-10 shadow-md hover:shadow-lg"
+        className="w-96 h-11 bg-slate-50 border outline-none p-6 text-black mt-10 shadow-md hover:shadow-lg"
         placeholder="검색어를 입력해주세요"
         onChange={onChangeInput}
         onKeyUp={onKeyUp}
